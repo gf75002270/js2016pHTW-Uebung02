ae5dbd45e5501edd7c67cacd4875cacbd40be70d
gf75002270 <mbongni2010@gmail.com>
2017-01-30 22:44:28 +0100

    Starter Poker Game
    Three cards permutation with javascript.Playing somehow with codes.Enjoy
    and improve.

---
+++ js/knuth-fisher-yates.js
@@ -0,0 +1,126 @@
+var first_card = []
+var second_card = []
+var third_card = []
+
+var DEFAULT_PURSE = 400;
+
+window.onload = function(){
+    document.getElementById("purse").innerHTML = DEFAULT_PURSE;
+}
+
+function createCards(){
+    first_card.push("king_of_hearth");
+    first_card.push("king_of_clubs");
+    first_card.push("king_of_spades");
+    first_card.push("king_of_diamonds");
+    second_card.push("queen_of_hearth");
+    second_card.push("queen_of_clubs");
+    second_card.push("queen_of_spades");
+    second_card.push("queen_of_diamonds");
+    third_card.push("jack_of_hearth");
+    third_card.push("jack_of_clubs");
+    third_card.push("jack_of_spades");
+    third_card.push("jack_of_diamonds");
+}
+
+function game(){
+    
+    first_card_shuffle = shuffle(first_card);
+    second_card_shuffle = shuffle(second_card);
+    third_card_shuffle = shuffle(third_card);
+    
+    var user_bid = parseInt(document.getElementById("bid").value);
+    var user_purse = parseInt(document.getElementById("purse").innerHTML);
+	
+    if(user_bid <= user_purse && user_bid > 0){
+        user_purse = user_purse - user_bid;
+        document.getElementById("purse").innerHTML = user_purse;
+		
+		displayCards();
+		
+		var black = ["clubs", "spades"];
+		var red = ["diamonds", "hearth"];
+		var all = ["diamonds", "hearth", "clubs", "spades"];
+		var three_same_cards = false;
+		
+		for(i = 0; i < all.length - 1; i++){
+			if (first_card_shuffle[0].endsWith(all[i]) && second_card_shuffle[0].endsWith(all[i]) && third_card_shuffle[0].endsWith(all[i])){
+				three_same_cards = true;
+				document.getElementById("purse").innerHTML = user_purse + user_bid * 2;
+				alert("Sie haben dreimal dieselbe Farbe ! Sie bekommen : "  + user_bid * 2 + ".");
+			}
+		}
+			
+		if (three_same_cards === false){
+			
+			for(i = 0; i < red.length - 1; i++){
+				if((first_card_shuffle[0].endsWith(red[i]) || first_card_shuffle[0].endsWith(red[i + 1])) && (second_card_shuffle[0].endsWith(red[i]) || second_card_shuffle[0].endsWith(red[i + 1])) && (third_card_shuffle[0].endsWith(red[i]) || third_card_shuffle[0].endsWith(red[i + 1]))){
+					document.getElementById("purse").innerHTML = user_purse + user_bid;
+					alert("Sie bekommen ihren Einsatz von "  + user_bid + " zurück.");
+					break;
+				}
+			}
+		
+			for(i = 0; i < black.length - 1; i++){
+				if((first_card_shuffle[0].endsWith(black[i]) || first_card_shuffle[0].endsWith(black[i + 1])) && (second_card_shuffle[0].endsWith(black[i]) || second_card_shuffle[0].endsWith(black[i + 1])) && (third_card_shuffle[0].endsWith(black[i]) || third_card_shuffle[0].endsWith(black[i + 1]))){
+					document.getElementById("purse").innerHTML = user_purse + user_bid;
+					alert("Sie bekommen ihren Einsatz von "  + user_bid + " zurück.");
+					break;
+				}   
+			}
+		}
+    } else{
+		
+		if (user_purse === 0){
+			document.getElementById("cards").innerHTML = "<h1>Sie haben kein Geld mehr in ihrer Börse übrig.</h1>";
+			document.getElementById("cards").innerHTML += "<br/><input type='button' value='Neu starten ?' onclick='restart()'/>";
+		} else {
+			if (user_bid === 0) {
+				alert("Der Einsatz muss höher als 0 sein !");
+			} else {
+				alert("Sie haben nicht genügend Geld in ihrer Börse.");
+			}
+		}
+    }   
+}
+
+function displayCards(){
+	
+    var first_card_image = first_card_shuffle[0] + ".png";
+    var second_card_image = second_card_shuffle[0] + ".png";
+    var third_card_image = third_card_shuffle[0] + ".png";
+    
+    document.getElementById("cards").innerHTML = "";
+    document.getElementById("cards").innerHTML += "<img src='pictures/cards/" + first_card_image + "' alt='Erste Karte'>";
+    document.getElementById("cards").innerHTML += "<img src='pictures/cards/" + second_card_image + "' alt='Zweite Karte'>";
+    document.getElementById("cards").innerHTML += "<img src='pictures/cards/" + third_card_image + "' alt='Dritte Karte'>";
+
+}
+
+function shuffle(array) {
+  var copy = [], n = array.length, i;
+
+  // While there remain elements to shuffle…
+  while (n) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * n--);
+
+    // And move it to the new array.
+    copy.push(array.splice(i, 1)[0]);
+  }
+
+  return copy;
+}
+
+function restart(){
+	document.getElementById("cards").innerHTML = "";
+	window.onload();
+	createCards();
+	game();
+}
+
+function main() {
+	createCards();
+    game();
+}
